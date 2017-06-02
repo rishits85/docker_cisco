f409419231c163f2d662b3c62eb45acdbdf5e2e5
Rishit Shah <Rishit Shah>
2017-06-02 15:36:58 -0700

    server.js updated

--- docker_app/artifacts/server.js
+++ docker_app/artifacts/server.js
@@ -29,18 +29,29 @@ console.error = console.log;
 
 
 // Load the http module to create an http server.
-var http = require('http');
+//var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  console.log("Request from " + (request.headers['x-forwarded-for'] || request.connection.remoteAddress))
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!\n");
-  console.log("Response sent..")
+
+
+//Restify server
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000, "0.0.0.0");
+//server.listen(8000, "0.0.0.0");
 
 // Put a friendly message on the terminal
-console.log("Server running at http://0.0.0.0:8000/");
+//console.log("Server running at http://0.0.0.0:8000/");
